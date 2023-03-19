f79c40dcac474bf6f887ae22b7c4aab652897011
KidJoe1412 <KidJoe1412@163.com>
2023-03-19 08:28:34 +0800

    2023/3/19

--- src/leetcode/Q207.java
+++ src/leetcode/Q207.java
@@ -19,44 +19,80 @@ import java.util.Queue;
  */
 
 // BFS做法
+//class Q207 {
+//    public boolean canFinish(int numCourses, int[][] prerequisites) {
+//        List<List<Integer>> ans = new ArrayList<>(); // 邻接表
+//        int[] inDgrees = new int[numCourses]; // 入度表
+//        Queue<Integer> queue = new LinkedList<>(); // 队列
+//
+//        // 初始化
+//        for (int i = 0; i < numCourses; i++) {
+//            ans.add(new ArrayList<>());
+//        }
+//
+//        // 邻接表和入度数组赋值
+//        for (int[] level : prerequisites) {
+//            ans.get(level[1]).add(level[0]);
+//            inDgrees[level[0]]++;
+//        }
+//
+//        // 入度为0的结点入队列
+//        for (int i = 0; i < numCourses; i++) {
+//            if (inDgrees[i] == 0)
+//                queue.add(i);
+//        }
+//
+//        // BFS遍历
+//        while (!queue.isEmpty()){
+//            int v = queue.poll(); // 弹出的结点
+//            numCourses--;
+//            // 把以v为前驱的结点的入度减1
+//            for (int level : ans.get(v)) {
+//                if (--inDgrees[level] == 0)
+//                    queue.add(level);
+//            }
+//        }
+//        return numCourses == 0;
+//    }
+//}
+
+// DFS做法
 class Q207 {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         List<List<Integer>> ans = new ArrayList<>(); // 邻接表
-        int[] inDgrees = new int[numCourses]; // 入度表
-        Queue<Integer> queue = new LinkedList<>(); // 队列
+        // 0代表未遍历过，-1代表已经遍历过，1代表正在遍历（即出现环了）
+        int[] flags = new int[numCourses];
 
         // 初始化
         for (int i = 0; i < numCourses; i++) {
             ans.add(new ArrayList<>());
+            flags[i] = 0;
         }
 
-        // 邻接表和入度数组赋值
+        // 邻接表赋值
         for (int[] level : prerequisites) {
             ans.get(level[1]).add(level[0]);
-            inDgrees[level[0]]++;
         }
 
-        // 入度为0的结点入队列
         for (int i = 0; i < numCourses; i++) {
-            if (inDgrees[i] == 0)
-                queue.add(i);
+            if (!dfs(ans,flags,i))
+                return false;
         }
-
-        // BFS遍历
-        while (!queue.isEmpty()){
-            int v = queue.poll(); // 弹出的结点
-            numCourses--;
-            // 把以v为前驱的结点的入度减1
-            for (int level : ans.get(v)) {
-                if (--inDgrees[level] == 0)
-                    queue.add(level);
-            }
+        return true;
+    }
+    private boolean dfs(List<List<Integer>> ans,int[] flags,int i){
+        // 退出条件
+        if (flags[i] == 1) return false;
+        if (flags[i] == -1) return true;
+        flags[i] = 1; // 当前正在访问此节点
+        for (Integer level : ans.get(i)) {
+            if (!dfs(ans,flags,level))
+                return false;
         }
-        return numCourses == 0;
+        flags[i] = -1; // 此节点被访问过啦
+        return true;
     }
 }
-
-
 
 
 
